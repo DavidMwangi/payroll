e9dbb01ff0f0d18268b6f959e16d08f9b08adf5f
DavidMwangi <david.mathenge98@gmail.com>
2017-05-09 16:01:02 +0300

    prompt function and check values

--- main.py
+++ main.py
@@ -2,12 +2,40 @@ def prompt():#prompts user for their details
 
     name = str(input("Full Name >> "))
 
-    hoursWorked = float(input("Hours Worked >> "))
+    def hours():
 
-    payRate = float(input("Pay Rate ($/hr) >> "))
+        hoursWorked = float(input("Hours Worked >> "))
+
+        if hoursWorked > 80:
+
+            print("Invalid Entry!")
+
+            hours()
+
+        else:
+
+            return hoursWorked
+
+    hoursWorked = hours()
+
+    def rate():
+
+        payRate = float(input("Pay Rate ($/hr) >> "))
+
+        if payRate > 10 and payRate < 75:
+
+            return payRate
+
+        else:
+
+            rate()
+
+    payRate = rate()
 
     details = (name, hoursWorked, payRate)
 
     return details
+
+print(prompt())
 
 
